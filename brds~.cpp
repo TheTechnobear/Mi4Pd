2e301ab39380f296fd6b53901ecaa1561a10597a
Mark Harris <mharris@technobear.com>
2017-07-10 13:37:39 +0200

    bit more brds code

--- brds~.cpp
+++ brds~.cpp
@@ -6,6 +6,11 @@
 #include <algorithm> 
 #include "braids/macro_oscillator.h"
 #include "braids/envelope.h"
+#include "braids/vco_jitter_source.h"
+
+// for signature_waveshaper, need abs
+inline int16_t abs(int16_t x) { return x <0.0f ? -x : x;}
+#include "braids/signature_waveshaper.h"
 
 static t_class *brds_tilde_class;
 
@@ -22,6 +27,16 @@ typedef struct _brds_tilde {
 
   braids::MacroOscillator osc;
   braids::Envelope envelope;
+  braids::SignatureWaveshaper ws;
+  braids::VcoJitterSource jitter_source;
+
+  int16_t   previous_pitch;
+  int16_t   previous_shape;
+  uint16_t  gain_lp;
+  bool      trigger_detected_flag;
+  bool      trigger_flag;
+  uint16_t  trigger_delay;
+
 
   t_float f_dummy;
 
@@ -35,6 +50,19 @@ typedef struct _brds_tilde {
 
   t_float f_ad_attack;
   t_float f_ad_decay;
+  t_float f_ad_mod_vca;
+  t_float f_ad_mod_timbre;
+  t_float f_ad_mod_colour;
+  t_float f_ad_mod_pitch;
+  t_float f_ad_mod_fm;
+
+  t_float f_paques;
+  t_float f_transposition;
+  t_float f_auto_trig;
+  t_float f_meta_modulation;
+  t_float f_vco_drift;
+  t_float f_vco_flatten;
+  t_float f_fm_cv_offset;
 
 
   // CLASS_MAINSIGNALIN  = in_sync;
@@ -55,93 +83,105 @@ extern "C"  {
 
   void brds_tilde_pitch(t_brds_tilde *x, t_floatarg f);
   void brds_tilde_shape(t_brds_tilde *x, t_floatarg f);
+  void brds_tilde_colour(t_brds_tilde *x, t_floatarg f);
+  void brds_tilde_timbre(t_brds_tilde *x, t_floatarg f);
 }
 
 
 
 
-// void RenderBlock() {
-//   static int16_t previous_pitch = 0;
-//   static int16_t previous_shape = 0;
-//   static uint16_t gain_lp;
-//   envelope.Update(f_attach,f_decay);
-//   uint32_t ad_value = envelope.Render();
-  
-//   if (ui.paques()) {
-//     osc.set_shape(MACRO_OSC_SHAPE_QUESTION_MARK);
-//   } else if (settings.meta_modulation()) {
-//     int16_t shape = adc.channel(3);
-//     shape -= settings.data().fm_cv_offset;
-//     if (shape > previous_shape + 2 || shape < previous_shape - 2) {
-//       previous_shape = shape;
-//     } else {
-//       shape = previous_shape;
-//     }
-//     shape = MACRO_OSC_SHAPE_LAST * shape >> 11;
-//     shape += settings.shape();
-//     if (shape >= MACRO_OSC_SHAPE_LAST_ACCESSIBLE_FROM_META) {
-//       shape = MACRO_OSC_SHAPE_LAST_ACCESSIBLE_FROM_META;
-//     } else if (shape <= 0) {
-//       shape = 0;
-//     }
-//     MacroOscillatorShape osc_shape = static_cast<MacroOscillatorShape>(shape);
-//     osc.set_shape(osc_shape);
-//     ui.set_meta_shape(osc_shape);
-//   } else {
-//     osc.set_shape(settings.shape());
-//   }
-  
-//   // Set timbre and color: CV + internal modulation.
-//   uint16_t parameters[2];
-//   for (uint16_t i = 0; i < 2; ++i) {
-//     int32_t value = settings.adc_to_parameter(i, adc.channel(i));
-//     Setting ad_mod_setting = i == 0 ? SETTING_AD_TIMBRE : SETTING_AD_COLOR;
-//     value += ad_value * settings.GetValue(ad_mod_setting) >> 5;
-//     CONSTRAIN(value, 0, 32767);
-//     parameters[i] = value;
-//   }
-//   osc.set_parameters(parameters[0], parameters[1]);
-  
-//   // Check if the pitch has changed to cause an auto-retrigger
-//   int32_t pitch_delta = pitch - previous_pitch;
-//   if (settings.data().auto_trig &&
-//       (pitch_delta >= 0x40 || -pitch_delta >= 0x40)) {
-//     trigger_detected_flag = true;
-//   }
-//   previous_pitch = pitch;
-  
-//   pitch += jitter_source.Render(settings.vco_drift());
-//   pitch += ad_value * settings.GetValue(SETTING_AD_FM) >> 7;
-  
-//   if (pitch > 16383) {
-//     pitch = 16383;
-//   } else if (pitch < 0) {
-//     pitch = 0;
-//   }
-  
-//   if (settings.vco_flatten()) {
-//     pitch = Interpolate88(lut_vco_detune, pitch << 2);
-//   }
-//   osc.set_pitch(pitch + settings.pitch_transposition());
 
-//   if (trigger_flag) {
-//     osc.Strike();
-//     envelope.Trigger(ENV_SEGMENT_ATTACK);
-//     trigger_flag = false;
+// puredata methods implementation -start
+t_int* brds_tilde_perform(t_int *w)
+{
+  t_brds_tilde *x = (t_brds_tilde *)(w[1]);
+  t_sample  *in_sync  =    (t_sample *)(w[2]);
+  t_sample  *out =    (t_sample *)(w[3]);
+  int n =  (int)(w[4]);
+
+  x->envelope.Update(int(x->f_ad_attack * 8.0f ) , int(x->f_ad_decay * 8.0f) );
+  uint32_t ad_value = x->envelope.Render();
+
+
+  if (x->f_paques) {
+    x->osc.set_shape(braids::MACRO_OSC_SHAPE_QUESTION_MARK);
+  } else if (x->f_meta_modulation) {
+    int16_t shape = (braids::MacroOscillatorShape) (x->f_shape * braids::MACRO_OSC_SHAPE_LAST);
+    shape -= x->f_fm_cv_offset;
+    if (shape > x->previous_shape + 2 || shape < x->previous_shape - 2) {
+      x->previous_shape = shape;
+    } else {
+      shape = x->previous_shape;
+    }
+    shape = braids::MACRO_OSC_SHAPE_LAST * shape >> 11;
+    shape += x->f_shape;
+    if (shape >= braids::MACRO_OSC_SHAPE_LAST_ACCESSIBLE_FROM_META) {
+      shape = braids::MACRO_OSC_SHAPE_LAST_ACCESSIBLE_FROM_META;
+    } else if (shape <= 0) {
+      shape = 0;
+    }
+    braids::MacroOscillatorShape osc_shape = static_cast<braids::MacroOscillatorShape>(shape);
+    x->osc.set_shape(osc_shape);
+  } else {
+    x->osc.set_shape((braids::MacroOscillatorShape) (x->f_shape * braids::MACRO_OSC_SHAPE_LAST));
+  }
+  
+  int32_t timbre = int(x->f_timbre * 32768.0f);
+  timbre += ad_value * x->f_ad_mod_timbre;
+  int32_t colour = int(x->f_colour * 32768.0f);
+  colour += ad_value * x->f_ad_mod_colour;
+  x->osc.set_parameters(constrain(timbre,0,32768), constrain(colour,0,32768));
+
+
+  int32_t pitch = x->f_pitch;
+  // if (!settings.meta_modulation()) {
+  //   pitch += settings.adc_to_fm(adc.channel(3));
+  // }
+
+  // Check if the pitch has changed to cause an auto-retrigger
+  int32_t pitch_delta = pitch - x->previous_pitch;
+  if (x->f_auto_trig &&
+      (pitch_delta >= 0x40 || -pitch_delta >= 0x40)) {
+    x->trigger_detected_flag = true;
+  }
+  x->previous_pitch = pitch; 
+  
+  // pitch += jitter_source.Render(x->f_vco_drift);
+  pitch += ad_value * x->f_ad_mod_pitch;
+  
+  if (pitch > 16383) {
+    pitch = 16383;
+  } else if (pitch < 0) {
+    pitch = 0;
+  }
+  
+//   if (x->f_vco_flatten) {
+//     pitch = Interpolate88(braids::lut_vco_detune, pitch << 2);
 //   }
-  
-//   uint8_t* sync_buffer = sync_samples[render_block];
-//   int16_t* render_buffer = audio_samples[render_block];
-  
-//   if (settings.GetValue(SETTING_AD_VCA) != 0
-//     || settings.GetValue(SETTING_AD_TIMBRE) != 0
-//     || settings.GetValue(SETTING_AD_COLOR) != 0
-//     || settings.GetValue(SETTING_AD_FM) != 0) {
-//     memset(sync_buffer, 0, kBlockSize);
-//   }
-  
-//   osc.Render(sync_buffer, render_buffer, kBlockSize);
-  
+  x->osc.set_pitch(pitch + x->f_transposition);
+
+  if (x->trigger_flag) {
+    x->osc.Strike();
+    x->envelope.Trigger(braids::ENV_SEGMENT_ATTACK);
+    x->trigger_flag = false;
+  }
+
+  bool sync_zero = x->f_ad_mod_vca!=0  || x->f_ad_mod_timbre !=0 || x->f_ad_mod_colour !=0 || x->f_ad_mod_fm !=0; 
+
+  uint8_t* sync = new uint8_t[n];
+  int16_t* outint = new int16_t[n];
+
+  for (int i = 0; i < n; i++) {
+    if(sync_zero) sync[i] = 0;
+    else sync[i] = in_sync[i] * (1<<8) ;
+  }
+
+  x->osc.Render(sync, outint, n);
+
+  for (int i = 0; i < n; i++) {
+    out[i] = outint[i] / 65536.0f ;
+  }
+
 //   // Copy to DAC buffer with sample rate and bit reduction applied.
 //   int16_t sample = 0;
 //   size_t decimation_factor = decimation_factors[settings.data().sample_rate];
@@ -157,37 +197,24 @@ extern "C"  {
 //     int16_t warped = ws.Transform(sample);
 //     render_buffer[i] = Mix(sample, warped, signature);
 //   }
-//   render_block = (render_block + 1) % kNumBlocks;
-// }
 
+  // bool trigger_detected = gate_input.raised();
+  // sync_samples[playback_block][current_sample] = trigger_detected;
+  // trigger_detected_flag = trigger_detected_flag | trigger_detected;
+  
+  // if (trigger_detected_flag) {
+  //   trigger_delay = settings.trig_delay()
+  //       ? (1 << settings.trig_delay()) : 0;
+  //   ++trigger_delay;
+  //   trigger_detected_flag = false;
+  // }
+  // if (trigger_delay) {
+  //   --trigger_delay;
+  //   if (trigger_delay == 0) {
+  //     trigger_flag = true;
+  //   }
+  // }
 
-// puredata methods implementation -start
-t_int* brds_tilde_perform(t_int *w)
-{
-  t_brds_tilde *x = (t_brds_tilde *)(w[1]);
-  t_sample  *in_sync  =    (t_sample *)(w[2]);
-  t_sample  *out =    (t_sample *)(w[3]);
-  int n =  (int)(w[4]);
-
-  x->envelope.Update(int(x->f_ad_attack * 8.0f ) , int(x->f_ad_decay * 8.0f) );
-  uint32_t ad_value = x->envelope.Render();
-
-  x->osc.set_pitch(x->f_pitch);
-  x->osc.set_shape( (braids::MacroOscillatorShape) (x->f_shape * braids::MACRO_OSC_SHAPE_LAST)) ;
-  x->osc.set_parameters(constrain(int(x->f_timbre * 32768.0f),0,32768), constrain(int(x->f_colour *32768.0f),0,32768)); 
-
-  uint8_t* sync = new uint8_t[n];
-  int16_t* outint = new int16_t[n];
-  for (int i = 0; i < n; i++) {
-    sync[i] = in_sync[i] * (1<<8) ;
-  }
-
-
-  x->osc.Render(sync, outint, n);
-
-  for (int i = 0; i < n; i++) {
-    out[i] = outint[i] / 65536.0f ;
-  }
 
   delete [] sync;
   delete [] outint;
@@ -215,16 +242,42 @@ void *brds_tilde_new(t_floatarg f)
 {
   t_brds_tilde *x = (t_brds_tilde *) pd_new(brds_tilde_class);
 
-  x->f_shape= 0.0f;
-  x->f_pitch = f;
+  x->previous_pitch = 0;
+  x->previous_shape = 0;
+  x->gain_lp = 0;
+  x->trigger_detected_flag = false;
+  x->trigger_flag = false;
+  x->trigger_delay = 0;
+
+
+  x->f_dummy = f;
+  x->f_shape = 0.0f;
+  x->f_pitch = 0.0f;
+  x->f_trig = 0.0f;
+  x->t_fm = 0.0f;
+  x->t_modulation = 0.0f;
+  x->f_colour = 0.0f;
+  x->f_timbre = 0.0f;
+  x->f_ad_attack = 0.0f;
+  x->f_ad_decay = 0.0f;
+  x->f_ad_mod_vca = 0.0f;
+  x->f_ad_mod_timbre = 0.0f;
+  x->f_ad_mod_colour = 0.0f;
+  x->f_ad_mod_pitch = 0.0f;
+  x->f_ad_mod_fm = 0.0f;
+  x->f_paques = 0.0f;
+  x->f_transposition = 0.0f;
+  x->f_auto_trig = 0.0f;
+  x->f_meta_modulation = 0.0f;
+  x->f_vco_drift = 0.0f;
+  x->f_vco_flatten = 0.0f;
+  x->f_fm_cv_offset = 0.0f;
 
   x->x_in_shape  = floatinlet_new (&x->x_obj, &x->f_shape);
   x->x_in_pitch  = floatinlet_new (&x->x_obj, &x->f_pitch);
   x->x_out   = outlet_new(&x->x_obj, &s_signal);
 
   x->osc.Init();
-  x->osc.set_pitch(x->f_pitch);
-  x->osc.set_shape( (braids::MacroOscillatorShape) (x->f_shape * braids::MACRO_OSC_SHAPE_LAST)) ;
   return (void *)x;
 }
 
@@ -235,6 +288,14 @@ void brds_tilde_pitch(t_brds_tilde *x, t_floatarg f)
 void brds_tilde_shape(t_brds_tilde *x, t_floatarg f)
 {
   x->f_shape = f;
+}
+void brds_tilde_colour(t_brds_tilde *x, t_floatarg f)
+{
+  x->f_colour = f;
+}
+void brds_tilde_timbre(t_brds_tilde *x, t_floatarg f)
+{
+  x->f_timbre = f;
 }
 
 void brds_tilde_setup(void) {
@@ -255,6 +316,12 @@ void brds_tilde_setup(void) {
                   A_DEFFLOAT, A_NULL);
   class_addmethod(brds_tilde_class,
                   (t_method) brds_tilde_shape, gensym("shape"),
+                  A_DEFFLOAT, A_NULL);
+  class_addmethod(brds_tilde_class,
+                  (t_method) brds_tilde_colour, gensym("colour"),
+                  A_DEFFLOAT, A_NULL);
+  class_addmethod(brds_tilde_class,
+                  (t_method) brds_tilde_timbre, gensym("timbre"),
                   A_DEFFLOAT, A_NULL);
 }
 // puredata methods implementation - end
