347db7fec715c24a8b85b24d396745b8cce037f6
Mark Harris <mharris@technobear.com>
2017-07-30 12:39:18 +0200

    wrps:add missing params

--- wrps~.cpp
+++ wrps~.cpp
@@ -6,17 +6,36 @@
 
 #include "warps/dsp/modulator.h"
 
+
+inline float constrain(float v, float vMin, float vMax) {
+  return std::max<float>(vMin,std::min<float>(vMax, v));
+}
+
+inline short TO_SHORTFRAME(float v)   { return (short (v * 16384.0f));}
+inline float FROM_SHORTFRAME(short v) { return (float(v) / 16384.0f); }
+
+
+
 static t_class *wrps_tilde_class;
 
 typedef struct _wrps_tilde {
-  t_object  x_obj;
+  t_object x_obj;
 
-  t_float  f_dummy;
+  t_float f_dummy;
 
-  t_float  f_drive1;
-  t_float  f_drive2;
-  t_float  f_algo;
-  t_float  f_density;
+  t_float f_shape;
+  t_float f_drive1;
+  t_float f_drive2;
+  t_float f_algo;
+  t_float f_param;
+
+  //todo
+  t_float f_bypass;
+  t_float f_easter_egg;
+  t_float f_frequency_shift_pot;
+  t_float f_frequency_shift_cv;
+  t_float f_phase_shift;
+  t_float f_note;  
 
   // CLASS_MAINSIGNALIN  = in_left;
   t_inlet*  x_in_right;
@@ -38,10 +57,11 @@ extern "C"  {
   void wrps_tilde_free(t_wrps_tilde *x);
   void* wrps_tilde_new(t_floatarg f);
   void wrps_tilde_setup(void);
+  void wrps_tilde_shape(t_wrps_tilde *x, t_floatarg f);
   void wrps_tilde_drive1(t_wrps_tilde *x, t_floatarg f);
   void wrps_tilde_drive2(t_wrps_tilde *x, t_floatarg f);
   void wrps_tilde_algo(t_wrps_tilde *x, t_floatarg f);
-  void wrps_tilde_density(t_wrps_tilde *x, t_floatarg f);
+  void wrps_tilde_param(t_wrps_tilde *x, t_floatarg f);
 }
 
 // puredata methods implementation -start
@@ -62,21 +82,35 @@ t_int *wrps_tilde_render(t_int *w)
     x->obuf = new warps::ShortFrame[x->iobufsz];
   }
 
-  x->processor.mutable_parameters()->channel_drive[0]=x->f_drive1;
-  x->processor.mutable_parameters()->channel_drive[1]=x->f_drive2;
-  x->processor.mutable_parameters()->modulation_parameter=x->f_density;
-  x->processor.mutable_parameters()->modulation_algorithm=x->f_algo;
+  int shape = int(constrain(x->f_shape * 3.0f,0.0,3.0));
+  float algo = constrain(x->f_algo,0.0f,1.0f);
+
+  x->processor.mutable_parameters()->carrier_shape = shape;
+  x->processor.mutable_parameters()->channel_drive[0]=constrain(x->f_drive1,0.0f,1.0f);
+  x->processor.mutable_parameters()->channel_drive[1]=constrain(x->f_drive2,0.0f,1.0f);
+  x->processor.mutable_parameters()->modulation_parameter=constrain(x->f_param,0.0f,1.0f);
+  x->processor.mutable_parameters()->modulation_algorithm=algo;
+  x->processor.set_bypass(x->f_bypass > 0.5);
+
+  // easter egg mode, not exposed yet
+  x->processor.set_easter_egg(x->f_easter_egg > 0.5);
+  x->processor.mutable_parameters()->frequency_shift_pot = constrain(x->f_frequency_shift_pot,0.0f,1.0f);
+  x->processor.mutable_parameters()->frequency_shift_cv = constrain(x->f_frequency_shift_cv,0.0f,1.0f);
+  x->processor.mutable_parameters()->phase_shift = constrain(x->f_phase_shift,0.0f,1.0f);
+  x->processor.mutable_parameters()->note = constrain(x->f_note,0.0f,1.0f);  
+
+
 
   for (int i = 0; i < n; i++) {
-    x->ibuf[i].l = in_left[i] * 1024;
-    x->ibuf[i].r = in_right[i] * 1024;
+    x->ibuf[i].l = TO_SHORTFRAME(in_left[i]);
+    x->ibuf[i].r = TO_SHORTFRAME(in_right[i]);
   }
 
   x->processor.Process(x->ibuf, x->obuf,n);
 
   for (int i = 0; i < n; i++) {
-    out_left[i] = x->obuf[i].l / 1024.0f;
-    out_right[i] = x->obuf[i].r / 1024.0f;
+    out_left[i]  = FROM_SHORTFRAME(x->obuf[i].l);
+    out_right[i] = FROM_SHORTFRAME(x->obuf[i].r);
   }
 
   return (w + 7); // # args + 1
@@ -107,11 +141,21 @@ void *wrps_tilde_new(t_floatarg f)
   x->iobufsz = 64;
   x->ibuf = new warps::ShortFrame[x->iobufsz];
   x->obuf = new warps::ShortFrame[x->iobufsz];
+  x->f_dummy = f;
 
-  x->f_drive1 = 1.0f;
-  x->f_drive2 = 1.0f;
+  x->f_shape  = 0.0f;
+  x->f_drive1 = 0.0f;
+  x->f_drive2 = 0.0f;
   x->f_algo   = 0.5f;
-  x->f_density = 0.5f;
+  x->f_param  = 0.5f;
+
+  x->f_bypass=0.0f;
+  x->f_easter_egg=0.0f;
+  x->f_frequency_shift_pot=0.0f;
+  x->f_frequency_shift_cv=0.0f;
+  x->f_phase_shift=0.0f;
+  x->f_note=0.0f;  
+
 
   x->x_in_right   = inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_signal, &s_signal);
   x->x_out_left   = outlet_new(&x->x_obj, &s_signal);
@@ -129,9 +173,9 @@ void wrps_tilde_drive2(t_wrps_tilde *x, t_floatarg f)
 {
   x->f_drive2 = f;
 }
-void wrps_tilde_density(t_wrps_tilde *x, t_floatarg f)
+void wrps_tilde_param(t_wrps_tilde *x, t_floatarg f)
 {
-  x->f_density= f;
+  x->f_param= f;
 }
 void wrps_tilde_algo(t_wrps_tilde *x, t_floatarg f)
 {
@@ -159,7 +203,7 @@ void wrps_tilde_setup(void) {
                   (t_method) wrps_tilde_drive2, gensym("drive2"),
                   A_DEFFLOAT, A_NULL);
   class_addmethod(wrps_tilde_class,
-                  (t_method) wrps_tilde_density, gensym("density"),
+                  (t_method) wrps_tilde_param, gensym("param"),
                   A_DEFFLOAT, A_NULL);
   class_addmethod(wrps_tilde_class,
                   (t_method) wrps_tilde_algo, gensym("algo"),
